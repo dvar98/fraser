dbc8db7a579c183bdc1ccd8f1798bbd8d19f61bf
dvar98 <100358915+dvar98@users.noreply.github.com>
2023-04-10 22:29:30 -0500

    Update and rename Ejercicio2.java to Main.java

--- Main.java
+++ Main.java
@@ -14,22 +14,26 @@ public class Ejercicio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    
+     ejercicio5  
+//        ColaLetras cola = new ColaLetras();
+//        String entrada = "AAABCDEFFFGHIJJJJKLMNNNNNOPQQQQRSTVWWWXXYYYZZZZZ";
+//        cola.insertar(entrada);
+//        System.out.print("Cola de letras: ");
+//        cola.mostrar();
+//        System.out.println();
+        
+    ejercicio4
+//        String cadena = "anita lava la tina";
+//        boolean esPalindromo = esPalindromo(cadena);
+//        if (esPalindromo) {
+//            System.out.println("La cadena " + cadena + " es un palíndromo.");
+//        } else {
+//            System.out.println("La cadena " + cadena + " no es un palíndromo.");
+//        }
         
         
-        
-        
-
-        CadenaXY cadena1 = new CadenaXY("OSOS&OSOS");
-        CadenaXY cadena2 = new CadenaXY("ABBDYCA&ACYDBBA");
-
-        System.out.println(cadena1.esFormaXY_Pila()); // Imprime false
-        System.out.println(cadena1.esFormaXY_Cola()); // Imprime false
-        System.out.println(cadena1.esFormaXY_Array()); // Imprime false
-
-        System.out.println(cadena2.esFormaXY_Pila()); // Imprime true
-        System.out.println(cadena2.esFormaXY_Cola()); // Imprime true
-        System.out.println(cadena2.esFormaXY_Array()); // Imprime true
-
+    ejercicio3
 //        Lista lista = new Lista(5);
 //        lista.agregar(3);
 //        lista.agregar(3);
@@ -60,31 +64,9 @@ public class Ejercicio2 {
 //
 //        System.out.print("Pila con los elementos de la lista ordenada: ");
 //        pila.imprimir();
-//
-//        Cola cola = new Cola(5);
-//        cola.agregarRepetidos(pila, valor);
-//
-//        System.out.print("Cola con los elementos iguales a 3 de la pila: ");
-//        cola.imprimir();
-//
-//        pila.retirar(valor);
-//        System.out.print("Pila con los elementos de la lista ordenada, sin el valor "+valor+": ");
-//        pila.imprimir();
-//        ColaU colaEstudiantes = new ColaU();
-//        colaEstudiantes.addTicket("Juan");
-//        colaEstudiantes.addTicket("Maria");
-//        colaEstudiantes.addTicket("123");
-//        colaEstudiantes.addTicket("Pedro");
-//        colaEstudiantes.addTicket("Ana");
-//        colaEstudiantes.mostrarCola();
-//        colaEstudiantes.removeAll();
-//        colaEstudiantes.mostrarCola();
-//        colaEstudiantes.remove();
-//        Pila[] pilas = new Pila[10];
-//        for (int i = 1; i <= 9; i++) {
-//            pilas[i] = new Pila();
-//        }
-//
+
+    ejercicio2
+    
 //        pilas[0].apilar(1);
 //        pilas[0].apilar(2);
 //        pilas[0].apilar(3);
@@ -111,7 +93,35 @@ public class Ejercicio2 {
 //            } else {
 //                System.out.println("No se encontro el dato " + datoB + " en la pila " + i);
 //            }
+//        }    
+      
+    ejercicio1
+    
+//        Cola cola = new Cola(5);
+//        cola.agregarRepetidos(pila, valor);
+//
+//        System.out.print("Cola con los elementos iguales a 3 de la pila: ");
+//        cola.imprimir();
+//
+//        pila.retirar(valor);
+//        System.out.print("Pila con los elementos de la lista ordenada, sin el valor "+valor+": ");
+//        pila.imprimir();
+//        ColaU colaEstudiantes = new ColaU();
+//        colaEstudiantes.addTicket("Juan");
+//        colaEstudiantes.addTicket("Maria");
+//        colaEstudiantes.addTicket("123");
+//        colaEstudiantes.addTicket("Pedro");
+//        colaEstudiantes.addTicket("Ana");
+//        colaEstudiantes.mostrarCola();
+//        colaEstudiantes.removeAll();
+//        colaEstudiantes.mostrarCola();
+//        colaEstudiantes.remove();
+//        Pila[] pilas = new Pila[10];
+//        for (int i = 1; i <= 9; i++) {
+//            pilas[i] = new Pila();
 //        }
+    
+
     }
 
 }
